ec09df2919ac1a1bef1d69bf0ff13e2e8f2fc6d6
Mian Muhammad <mianmuhammad660@gmail.com>
2024-08-24 22:41:40 +0500

    📦 NEW: role based access control

--- src/express/routes/v1/auth/index.ts
+++ src/express/routes/v1/auth/index.ts
@@ -3,6 +3,7 @@ import log from '../../../../utils/log';
 import authService from '../../../../services/auth-service';
 import validate from '../../../middlewares/validations/validate';
 import { registerSchema, loginSchema } from '../../../middlewares/validations/schemas';
+import verifyTokenAndRole from '../../../middlewares/verifyTokenAndRole';
 
 const router = Router();
 
@@ -23,15 +24,25 @@ const authRoutes = () => {
 	router.post('/login', validate(loginSchema), async (req: Request, res: Response) => {
 		try {
 			const data: any = await authService.loginUser(req.body);
+			const userRole = data?.data?.role;
 			const idToken = data?.data?.idToken;
 
-			if (idToken) {
+			if (idToken && userRole) {
 				res.cookie('token', idToken, {
 					httpOnly: true,
+					secure: true,
+					sameSite: 'strict',
+				});
+
+				res.cookie('userRole', userRole, {
+					httpOnly: true,
+					secure: true,
+					sameSite: 'strict',
 				});
 			}
 
 			delete data.data.idToken;
+			delete data.data.role;
 
 			res.status(data.statusCode).send(data);
 		} catch (error: any) {
@@ -60,6 +71,19 @@ const authRoutes = () => {
 	router.post('/forgot-password', async (req: Request, res: Response) => {
 		try {
 			const data: any = await authService.resetPassword(req.body);
+			res.status(data.statusCode).send(data);
+		} catch (error: any) {
+			log.error(error);
+			res.status(500).send({
+				status: 500,
+				message: 'Internal Server Error',
+			});
+		}
+	});
+
+	router.get('/protected', verifyTokenAndRole, async (req: Request, res: Response) => {
+		try {
+			const data: any = await authService.protectedRoute();
 			res.status(data.statusCode).send(data);
 		} catch (error: any) {
 			log.error(error);
