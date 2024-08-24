864f8850d27fea0d8a5e299fcc4b919ae9b99527
Mian Muhammad <mianmuhammad660@gmail.com>
2024-08-24 16:53:33 +0500

    📦 NEW: zod validation middleware

---
+++ src/express/middlewares/validations/validate.ts
@@ -0,0 +1,16 @@
+import { Request, Response, NextFunction } from 'express';
+import * as z from 'zod';
+
+const validate = (schema: z.ZodSchema) => (req: Request, res: Response, next: NextFunction) => {
+	try {
+		schema.parse(req.body);
+		next();
+	} catch (error: any) {
+		const errors = error.errors.map((err: any) => ({ field: err.path[0], message: err.message }));
+		return res.status(400).json({
+			errors
+		});
+	}
+};
+
+export default validate;
