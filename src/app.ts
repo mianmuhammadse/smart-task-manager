f4a6bb70a99784713ebafe9b028b185dfb14d3d7
Mian Muhammad <mianmuhammad660@gmail.com>
2024-08-26 03:00:08 +0500

    IMPROVE: database connection refactoring

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,14 @@
 import setupServer from './express/server';
 import 'dotenv/config';
-import db from './db';
+import { connectDatabase } from './db';
 
 const PORT = process.env.PORT || 3000;
 
-console.log('Application starting ...')
+console.log('Application starting ...');
 
 export const app = setupServer();
 
-db.sequelize
-	.authenticate()
-	.then(async () => {
-		console.log('Connection has been established successfully.');
-	})
-	.catch((error) => {
-		console.log('Unable to connect to the database:', error);
-	});
-
 app.listen(PORT, () => {
 	console.log(`App started on port ${PORT}`);
+	connectDatabase();
 });
