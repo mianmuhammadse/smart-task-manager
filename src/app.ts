ef13900728a4bcb61daf17ea70e6ae6e8d872e8a
Mian Muhammad <mianmuhammad660@gmail.com>
2024-10-14 17:36:10 +0500

    NEW: app setup and database authentication mocking

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
 import setupServer from './express/server';
-import { connectDatabase } from './db';
+import db from './db';
 import 'dotenv/config';
 
 const PORT = process.env.PORT || 3000;
@@ -8,7 +8,11 @@ console.log('Application starting ...');
 
 const app = setupServer();
 
+db.sequelize
+	.authenticate()
+	.then(() => console.log('Connection has been established successfully.'))
+	.catch((error) => console.error('Unable to connect to the database:', error));
+
 app.listen(PORT, () => {
 	console.log(`App started on port ${PORT}`);
-	connectDatabase();
 });
--- src/db/index.ts
+++ src/db/index.ts
@@ -29,13 +29,4 @@ dbModelInitializer.forEach((initializer) => {
 User.hasMany(Task, { foreignKey: 'userId', as: 'tasks' });
 Task.belongsTo(User, { foreignKey: 'userId', as: 'user' });
 
-export const connectDatabase = async () => {
-	try {
-		await db.sequelize.authenticate();
-		console.log('Connection has been established successfully.');
-	} catch (error) {
-		console.error('Unable to connect to the database:', error);
-	}
-};
-
 export default db;
